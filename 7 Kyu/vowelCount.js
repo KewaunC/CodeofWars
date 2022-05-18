1bc680414c59117836c8ad08884246fce3b72f0f
Kewaun Cain <kewaunscain@gmail.com>
2022-05-18 18:34:02 -0400

    7 Kyu and updated 8 Kyu

---
+++ 7 Kyu/vowelCount.js
@@ -0,0 +1,17 @@
+
+function getCount(input) {
+  var vowelsCount = 0;
+  var inputLetters = input.split("");
+  
+  const vowels = ["a","e","i","o","u"];
+  
+  vowels.forEach(function(vowel) {
+    inputLetters.forEach(function(inpLetter) {
+      if (inpLetter === vowel) {
+        vowelsCount++;
+      }
+    });
+  });
+  
+  return vowelsCount;
+}
