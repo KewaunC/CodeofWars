d5f3235460e5114d6ba8b464bdaa218358d69f80
Kewaun Cain <kewaunscain@gmail.com>
2023-01-07 21:32:20 -0500

    New Kats

---
+++ 8 Kyu/schoolPaperwork.js
@@ -0,0 +1,9 @@
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+
+
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+  }
