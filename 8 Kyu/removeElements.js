f800969c0f48e2cf448f615b7783079db21b2b75
Kewaun Cain <kewaunscain@gmail.com>
2023-01-12 16:40:40 -0500

    Old Solution with For Loop

--- 8 Kyu/removeElements.js
+++ 8 Kyu/removeElements.js
@@ -1,5 +1,35 @@
 //Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
+/*
+Parameter
+arr which is comprised of elements
+
+Return
+I need to return every other element inside of the arr
+
+Example
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] 
+--> ["Keep", "Keep", "Keep", ...]
+
+LETS COUNT
+keep = Index 0
+remove = Index 1
+keep = Index 2
+remove = Index 3
+keep = Index 4
+
+Pseudo Code
+I need to loop through the array and delete the 1st, 3rd, 5th indexes
+
+*/
+
+function removeEveryOther(arr){
+    return arr.filter((_,index)=> index  % 2 ==0)
+}
+
+
+
+/*OLD CODE
 
 /*
 Parameter
@@ -24,12 +54,4 @@ I need to loop through the array and delete the 1st, 3rd, 5th indexes
 
 */
 
-
-function removeEveryOther(arr){
-    let newArr = []
-    
-      for(let i = 0; i < arr.length; i++ ){
-      
-       arr.splice(i, 1)
-    }
-  }
+*/
